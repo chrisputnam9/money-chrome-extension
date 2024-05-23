8f1b6b6bdf11dabecaccc37ea707835f4b59d2d1
Chris Putnam <chris@webfx.com>
2024-05-23 08:12:17 -0400

    v3 still in progress

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,44 +1,44 @@
 /* global chrome:readonly */
 (function () {
-	let URL = '';
-	let USERNAME = '';
-	let API_KEY = '';
+	let URL = "";
+	let USERNAME = "";
+	let API_KEY = "";
 
 	chrome.storage.sync.get(function (data) {
-		URL = 'url' in data ? data.url : '';
-		USERNAME = 'user' in data ? data.user : '';
-		API_KEY = 'key' in data ? data.key : '';
+		URL = "url" in data ? data.url : "";
+		USERNAME = "user" in data ? data.user : "";
+		API_KEY = "key" in data ? data.key : "";
 	});
 
 	// Listen for tab updates
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-		if (changeInfo.status !== 'complete') return false; // loading complete?
+		if (changeInfo.status !== "complete") return false; // loading complete?
 		if (tab.url.indexOf(URL) !== 0) return false; // URL matches app URL?
 
 		// At this point, we know a URL has loaded which matches our app URL
 		//  - So we now want to inject our content script
 		chrome.tabs.executeScript(tabId, {
-			file: 'src/content.js',
+			file: "src/content.js",
 		});
 	});
 
 	// Listen for messages
 	chrome.runtime.onMessage.addListener(function (message, sender) {
-		const name = 'name' in message ? message.name : false;
+		const name = "name" in message ? message.name : false;
 
 		// Requested to close window
-		if (name === 'cmp.money.close_window') {
+		if (name === "cmp.money.close_window") {
 			// Close tab that sent the message
 			if (sender.tab) {
 				chrome.tabs.remove(sender.tab.id);
 				return true;
 			}
 
-			throw new Error('Failed to close money app tab');
+			throw new Error("Failed to close money app tab");
 		}
 
 		// Options saved
-		if (name === 'options_saved') {
+		if (name === "options_saved") {
 			URL = message.data.url;
 			USERNAME = message.data.user;
 			API_KEY = message.data.key;
@@ -47,33 +47,33 @@
 
 	// Add Context Menu item - for selection
 	chrome.contextMenus.create({
-		id: 'addTransactionFromSelection',
-		title: 'Add Transaction From Selection',
-		contexts: ['selection'],
+		id: "addTransactionFromSelection",
+		title: "Add Transaction From Selection",
+		contexts: ["selection"],
 	});
 
 	// Add Context Menu item - for all others
 	chrome.contextMenus.create({
-		id: 'addTransactionFromPage',
-		title: 'Add Transaction From Page',
-		contexts: ['page', 'frame', 'browser_action', 'page_action'],
+		id: "addTransactionFromPage",
+		title: "Add Transaction From Page",
+		contexts: ["page", "frame", "browser_action", "page_action"],
 	});
 
 	// Listen for shortcut command
 	chrome.commands.onCommand.addListener(function (command) {
-		if (command === 'add-transaction-from-page') {
-			addTransaction('page');
+		if (command === "add-transaction-from-page") {
+			addTransaction("page");
 		}
 	});
 
 	// Listen for context menu click
 	chrome.contextMenus.onClicked.addListener(function (info) {
-		if (info.menuItemId === 'addTransactionFromSelection') {
-			addTransaction('selection');
+		if (info.menuItemId === "addTransactionFromSelection") {
+			addTransaction("selection");
 		}
 
-		if (info.menuItemId === 'addTransactionFromPage') {
-			addTransaction('page');
+		if (info.menuItemId === "addTransactionFromPage") {
+			addTransaction("page");
 		}
 	});
 
@@ -87,7 +87,7 @@
 			},
 			function (tabs) {
 				if (tabs.length === 0) {
-					throw new Error('Unable to get active tab for some reason');
+					throw new Error("Unable to get active tab for some reason");
 				}
 
 				const tab = tabs[0];
@@ -95,32 +95,34 @@
 				const pageUrl = tab.url;
 				let code;
 
-				if (context === 'selection') {
-					code = 'window.getSelection().toString()';
-				} else if (context === 'page') {
+				if (context === "selection") {
+					code = "window.getSelection().toString()";
+				} else if (context === "page") {
 					// Special handling for gmail
 					if (tab.url.match(/mail\.google\.com/)) {
-						code =
-							"(window.document.querySelector('table.Bs.nH.iY.bAt') || window.document.body).innerText";
+						code = () =>
+							window.document.querySelector(
+								"table.Bs.nH.iY.bAt"
+							) || window.document.body.innerText;
 					} else {
-						code = 'window.document.body.innerText';
+						code = () => window.document.body.innerText;
 					}
 				}
 
-				chrome.tabs.executeScript(
-					tab.id,
-					{
-						code,
+				chrome.scripting.executeScript({
+					injection: {
+						target: { tabId: tab.id },
+						func: code,
 					},
-					function (results) {
+					callback: function (results) {
 						const transactionContent = results[0];
 
-						fetch(URL + '/transaction/text?ajax=1', {
-							method: 'POST',
-							mode: 'cors',
-							cache: 'no-cache',
+						fetch(URL + "/transaction/text?ajax=1", {
+							method: "POST",
+							mode: "cors",
+							cache: "no-cache",
 							headers: {
-								'Content-Type': 'application/json',
+								"Content-Type": "application/json",
 							},
 							body: JSON.stringify({
 								authentication: {
@@ -135,26 +137,26 @@
 						})
 							.then((response) => response.json())
 							.then((response) => {
-								if ('location' in response) {
+								if ("location" in response) {
 									const location = response.location,
 										url = URL + location;
 
 									chrome.windows.create({
 										url,
 										focused: true,
-										type: 'popup',
+										type: "popup",
 									});
-								} else if ('error' in response) {
+								} else if ("error" in response) {
 									throw new Error(
-										'There has been an error: ' +
+										"There has been an error: " +
 											response.error
 									);
 								} else {
 									throw new Error(response);
 								}
 							});
-					}
-				);
+					},
+				});
 			}
 		);
 	} // End addTransaction
